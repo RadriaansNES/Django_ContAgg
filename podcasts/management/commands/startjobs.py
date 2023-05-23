e96b8bf763c3d67e672ae17903c331c9599a0dd8
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-05-23 10:53:51 -0400

    Base additional feed added.

--- podcasts/management/commands/startjobs.py
+++ podcasts/management/commands/startjobs.py
@@ -1,23 +1,34 @@
-from django.core.management.base import BaseCommand
 import feedparser
 from dateutil import parser
+from django.core.management.base import BaseCommand
 from podcasts.models import Episode
 
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        feed = feedparser.parse("https://realpython.com/podcasts/rpp/feed")
-        podcast_title = feed.channel.title
-        podcast_image = feed.channel.image["href"]
+def save_new_episodes(feed):
+    podcast_title = feed.channel.title
+    podcast_image = feed.channel.image["href"]
 
-        for item in feed.entries:
-            if not Episode.objects.filter(guid=item.guid).exists():
-                episode = Episode(
-                    title=item.title,
-                    description=item.description,
-                    pub_date=parser.parse(item.published),
-                    link=item.link,
-                    image=podcast_image,
-                    podcast_name=podcast_title,
-                    guid=item.guid,
-                )
-                episode.save()
+    for item in feed.entries: ## loop to check if episode ID is within database, and if not, to add it
+        if not Episode.objects.filter(guid=item.guid).exists():
+            episode = Episode(
+                title=item.title,
+                description=item.description,
+                pub_date=parser.parse(item.published),
+                link=item.link,
+                image=podcast_image,
+                podcast_name=podcast_title,
+                guid=item.guid,
+            )
+            episode.save()
+
+def fetch_realpython_episodes(): ## original feed
+    _feed = feedparser.parse("https://realpython.com/podcasts/rpp/feed")
+    save_new_episodes(_feed)
+
+def fetch_talkpython_episodes(): ## adding additional feed
+    _feed = feedparser.parse("https://talkpython.fm/episodes/rss")
+    save_new_episodes(_feed)
+
+class Command(BaseCommand): # calling functions
+    def handle(self, *args, **options):
+        fetch_realpython_episodes()
+        fetch_talkpython_episodes()
